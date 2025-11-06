8bb12083e98a55bd120fc7bfbed504a7aaf7c6d2
igorakimy <igorakimy@gmail.com>
2025-11-06 23:32:57 +0300

    feat: add appearance switcher

--- resources/js/components/app/app-sidebar-header.tsx
+++ resources/js/components/app/app-sidebar-header.tsx
@@ -1,6 +1,7 @@
 import { Breadcrumbs } from '@/components/breadcrumbs/breadcrumbs';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { type BreadcrumbItem } from '@/types';
+import AppearanceToggleTabs from '@/components/appearance/appearance-tabs';
 
 interface AppSidebarHeaderProps {
   breadcrumbs?: BreadcrumbItem[];
@@ -12,6 +13,9 @@ export function AppSidebarHeader({ breadcrumbs = [] }: AppSidebarHeaderProps) {
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-4">
         <SidebarTrigger className="-ml-1" />
         <Breadcrumbs breadcrumbs={breadcrumbs} />
+      </div>
+      <div className="flex w-full justify-end px-4">
+        <AppearanceToggleTabs />
       </div>
     </header>
   );
---
+++ resources/js/components/appearance/appearance-tabs.tsx
@@ -0,0 +1,49 @@
+import { Appearance, useAppearance } from '@/hooks/use-appearance';
+import { LucideIcon, Monitor, Moon, Sun } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
+
+interface AppearanceToggleTabsProps {
+  className?: string;
+}
+
+interface AppearanceTab {
+  value: Appearance;
+  icon: LucideIcon;
+  label: string;
+}
+
+export default function AppearanceToggleTabs({className = '', ...props}: AppearanceToggleTabsProps) {
+  const { appearance, updateAppearance } = useAppearance();
+
+  const tabs: AppearanceTab[] = [
+    {value: 'light', icon: Sun, label: 'Светлая'},
+    {value: 'dark', icon: Moon, label: 'Темная'},
+    {value: 'system', icon: Monitor, label: 'Системная'},
+  ];
+
+  return (
+    <div className={cn('inline-flex gap-1 rounded-lg bg-neutral-100 p-1 dark:bg-neutral-800', className)} {...props}>
+      {tabs.map(({value, icon: Icon, label}) => (
+        <Tooltip key={value}>
+          <TooltipTrigger asChild>
+            <button
+              onClick={() => updateAppearance(value)}
+              className={cn(
+                'flex items-center rounded-md px-2.5 py-1.5',
+                appearance === value
+                  ? 'bg-white shadow-xs dark:bg-neutral-700 dark:text-neutral-100'
+                  : 'text-neutral-500 hover:bg-neutral-200/60 hover:text-black dark:text-neutral-400 dark:hover:bg-neutral-700/60'
+              )}
+            >
+              <Icon className="-ml-0.5 h-4 w-4" />
+            </button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p className="font-normal">{label}</p>
+          </TooltipContent>
+        </Tooltip>
+      ))}
+    </div>
+  );
+}
